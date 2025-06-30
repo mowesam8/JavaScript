25e6d7f561918173fb86b23c677e57996b6a8a3a
Mohamed Wesam <mmohamedwesam111@gmail.com>
2025-06-30 22:04:25 +0300

    Push Assigments

---
+++ Week_1/Assigments-from-001-to-009/Assigment_03/main.js
@@ -0,0 +1,3 @@
+console.log("%cElzero %cWeb %cSchool", "color:red; font-size:40px", "color:green; font-size:40px; font-weight:bold", "color:white; font-size:40px; background:blue")
+
+
---
+++ Week_1/Assigments-from-001-to-009/Assigment_06/main.js
@@ -0,0 +1,6 @@
+// console.log("Iam In Console");
+// document.write("Iam In Page");
+
+/*console.log("Iam In Console");
+document.write("Iam In Page");*/
+
