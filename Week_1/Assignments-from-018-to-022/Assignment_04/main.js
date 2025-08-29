3dc8e6550a81438149c3360e50323b92a56bf430
Mohamed Wesam <mmohamedwesam111@gmail.com>
2025-08-29 23:28:43 +0300

    push tasks

---
+++ Week_1/Assignments-from-018-to-022/Assignment_04/main.js
@@ -0,0 +1,14 @@
+let points = 10;
+
+// Write Your Code Here
+points += true + true + true;
+console.log(points); // 13
+
+// Write Your Code Here
+points /= (true + true);
+points++;
+++points;
+points = points - (points % true);
+console.log(points); // 8;
+
+
---
+++ Week_1/Assignments-from-023-to-026/Assignment_01/main.js
@@ -0,0 +1,16 @@
+// Examples
+console.log(100_000); // 100000
+console.log(100000); // 100000
+console.log(5e4 + 5e4); // 100000
+
+// Your Solutions
+console.log(1e5); // 100000
+console.log(10 ** 5); // 100000
+console.log(Math.round(99999.6)); // 100000
+console.log(Math.ceil(99999.1)); // 100000
+console.log(Math.floor(100000.9)); // 100000
+console.log(Math.trunc(100000.7)); // 100000
+console.log(Math.pow(10, 5)); // 100000
+console.log(+100000.3.toFixed(0)); // 100000
+console.log(Math.max(100,2000,40000,100000,55000)); // 100000
+console.log(Math.min(200000,500000,100000,440000)); // 100000
